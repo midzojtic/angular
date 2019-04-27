559d4568ea06469f617b18872a5c36c3052a1d52
Mato <matopeicevic@yahoo.com>
2019-04-27 18:35:48 +0200

    aaaa1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = 'angular';
-  public screen: number;
+  title = 'angular';
+  screen: number;
 }
