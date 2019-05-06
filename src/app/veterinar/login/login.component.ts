1a431a0c6491aea1ae9c6a0d219cf2e8630cf5e4
Mato <matopeicevic@yahoo.com>
2019-05-06 22:37:11 +0200

    aaaa12

---
+++ src/app/veterinar/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  returnUrl: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+
+    this.router.navigate(['']);
+  }
+
+}
