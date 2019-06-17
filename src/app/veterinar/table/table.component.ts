6e299b2ee565656c11221c12ddedb3a674e04771
Marko <midzojtic@gmail.com>
2019-06-17 19:42:57 +0200

    added client

--- src/app/veterinar/table/table.component.ts
+++ src/app/veterinar/table/table.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {RestDto} from "../model/rest-dto.model";
-import {UserModel} from "../model/user.model";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {ActivatedRoute, Router} from "@angular/router";
 import {environment} from "../../../environments/environment";
@@ -20,12 +19,12 @@ export interface Food {
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-  dataSource: Food[] = [
-    {name: 'Yogurt', calories: 159, fat: 6, carbs: 24, protein: 4},
-    {name: 'Sandwich', calories: 237, fat: 9, carbs: 37, protein: 4},
-    {name: 'Eclairs', calories: 262, fat: 16, carbs: 24, protein: 6},
-    {name: 'Cupcakes', calories: 305, fat: 4, carbs: 67, protein: 4},
-    {name: 'Gingerbreads', calories: 356, fat: 16, carbs: 49, protein: 4},
+  dataSource: ClientModel[] = [
+    // {name: 'Yogurt', calories: 159, fat: 6, carbs: 24, protein: 4},
+    // {name: 'Sandwich', calories: 237, fat: 9, carbs: 37, protein: 4},
+    // {name: 'Eclairs', calories: 262, fat: 16, carbs: 24, protein: 6},
+    // {name: 'Cupcakes', calories: 305, fat: 4, carbs: 67, protein: 4},
+    // {name: 'Gingerbreads', calories: 356, fat: 16, carbs: 49, protein: 4},
   ];
   displayedColumns: string[] = ['name', 'calories', 'fat', 'carbs', 'protein'];
   model: ClientModel;
@@ -48,9 +47,10 @@ export class TableComponent implements OnInit {
   fetchClients() {
     console.log('Accessed onSubmit');
     this.http.get<RestDto<any>>(this.serverUrl + '/getClients').subscribe(
-      (response: RestDto<UserModel>) => {
+      (response: RestDto<Array<ClientModel>>) => {
         if (response.success) {
-          this.mapTable();
+          var clients: ClientModel[] = response.data;
+          this.mapTable(clients);
         } else {
           console.log('No client data')
 
@@ -68,8 +68,27 @@ export class TableComponent implements OnInit {
     }
   }
 
-  mapTable() {
+  mapTable(clients: ClientModel[]) {
 
+    var num: number = 0;
+    this.dataSource = new Array(clients.length);
+    this.dataSource = clients;
+    for (num = 0; num < clients.length; num++) {
+
+      var c = new ClientModel();
+
+      c.clientName = clients[num].clientName;
+      c.clientSurname = clients[num].clientSurname;
+      c.clientEmail = clients[num].clientEmail;
+      c.clientCell = clients[num].clientCell;
+
+    }
   }
+
+
+
+
+
+
 
 }
